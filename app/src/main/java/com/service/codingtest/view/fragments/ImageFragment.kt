f95f3dcafd85fc5451897fcce079a5d93fcb9c5e
kkeman <kkeman11@gmail.com>
2022-04-26 06:06:01 +0900

    keyword auto search

--- app/src/main/java/com/service/codingtest/view/fragments/ImageFragment.kt
+++ app/src/main/java/com/service/codingtest/view/fragments/ImageFragment.kt
@@ -1,10 +1,14 @@
 package com.service.codingtest.view.fragments
 
+
 import android.app.Application
 import android.os.Bundle
-import android.view.*
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.*
 import androidx.paging.LoadState
 import androidx.savedstate.SavedStateRegistryOwner
@@ -16,7 +20,12 @@ import com.service.codingtest.repository.DbImagePostRepository
 import com.service.codingtest.view.adapters.ImageAdapter
 import com.service.codingtest.view.adapters.ImageLoadStateAdapter
 import com.service.codingtest.viewmodel.ImageListViewModel
-//import com.service.codingtest.viewmodel.SharedViewModel
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.core.ObservableEmitter
+import io.reactivex.rxjava3.core.ObservableOnSubscribe
+import io.reactivex.rxjava3.core.Observer
+import io.reactivex.rxjava3.schedulers.Schedulers
 import kotlinx.android.synthetic.main.frag_image.*
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.FlowPreview
@@ -24,6 +33,7 @@ import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.flow.distinctUntilChangedBy
 import kotlinx.coroutines.flow.filter
+import java.util.concurrent.TimeUnit
 
 
 class ImageFragment : Fragment() {
@@ -34,8 +44,15 @@ class ImageFragment : Fragment() {
 
 //    private val model: SharedViewModel by activityViewModels()
 
-    class MainViewModelFactory(owner: SavedStateRegistryOwner, private val documentRepository :DbImagePostRepository) : AbstractSavedStateViewModelFactory(owner, null) {
-        override fun <T : ViewModel?> create(key: String, modelClass: Class<T>, handle: SavedStateHandle): T {
+    class MainViewModelFactory(
+        owner: SavedStateRegistryOwner,
+        private val documentRepository: DbImagePostRepository
+    ) : AbstractSavedStateViewModelFactory(owner, null) {
+        override fun <T : ViewModel?> create(
+            key: String,
+            modelClass: Class<T>,
+            handle: SavedStateHandle
+        ): T {
             @Suppress("UNCHECKED_CAST")
             return ImageListViewModel(handle, documentRepository) as T
         }
@@ -48,8 +65,14 @@ class ImageFragment : Fragment() {
     ): View? {
         val view = inflater.inflate(R.layout.frag_image, container, false)
         binding = FragImageBinding.bind(view)
-        binding.vm = ViewModelProvider(requireActivity(), MainViewModelFactory(this, DbImagePostRepository(
-            AppDB.getInstance(requireContext().applicationContext as Application), ImageAPI.create())))
+        binding.vm = ViewModelProvider(
+            requireActivity(), MainViewModelFactory(
+                this, DbImagePostRepository(
+                    AppDB.getInstance(requireContext().applicationContext as Application),
+                    ImageAPI.create()
+                )
+            )
+        )
             .get(ImageListViewModel::class.java)
 
         return binding.root
@@ -101,13 +124,40 @@ class ImageFragment : Fragment() {
         layout_swipe_refresh.setOnRefreshListener { adapter.refresh() }
     }
 
-    private fun initSearchEditText() =
-        et_search.setOnKeyListener(View.OnKeyListener { _, keyCode, event ->
-            if (event.action == KeyEvent.ACTION_DOWN && keyCode == KeyEvent.KEYCODE_ENTER) {
+    private fun initSearchEditText() {
+        val observableTextQuery =
+            Observable.create(ObservableOnSubscribe { emitter: ObservableEmitter<String>? ->
+                et_search.addTextChangedListener(object : TextWatcher {
+                    override fun onTextChanged(
+                        s: CharSequence?,
+                        start: Int,
+                        before: Int,
+                        count: Int
+                    ) {
+                    }
+
+                    override fun afterTextChanged(s: Editable?) {}
+                    override fun beforeTextChanged(
+                        s: CharSequence?, start: Int,
+                        count: Int, after: Int
+                    ) {
+                        emitter?.onNext(s.toString())
+                    }
+                })
+            })
+                .debounce(1, TimeUnit.SECONDS)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+
+        observableTextQuery.subscribe(object : Observer<String> {
+            override fun onComplete() {}
+            override fun onSubscribe(d: io.reactivex.rxjava3.disposables.Disposable?) {}
+            override fun onNext(t: String) {
                 binding.vm!!.showSubreddit()
-                return@OnKeyListener true
             }
-            false
+
+            override fun onError(e: Throwable?) {}
         })
+    }
 
 }
