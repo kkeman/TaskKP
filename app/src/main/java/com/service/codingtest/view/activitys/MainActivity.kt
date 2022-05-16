9199223bc3f134ae62cf5fdf1fc404f049e0b859
kkeman <kkeman11@gmail.com>
2022-05-17 01:50:37 +0900

    intro change

--- app/src/main/java/com/service/codingtest/view/activitys/MainActivity.kt
+++ app/src/main/java/com/service/codingtest/view/activitys/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.service.codingtest.view.activitys
 
 import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import com.service.codingtest.R
@@ -10,6 +11,7 @@ class MainActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         setContentView(R.layout.activity_main)
 
         val imageFragment: Fragment = ImageFragment()
